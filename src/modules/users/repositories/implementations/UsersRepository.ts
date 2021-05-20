735044236a1ae95094ab068cbad35386071391a1
kaynansc <kaynansc@gmail.com>
2021-05-19 22:06:56 -0300

    initial commit

---
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -0,0 +1,33 @@
+import { getRepository, Repository } from 'typeorm';
+
+import { IFindUserWithGamesDTO, IFindUserByFullNameDTO } from '../../dtos';
+import { User } from '../../entities/User';
+import { IUsersRepository } from '../IUsersRepository';
+
+export class UsersRepository implements IUsersRepository {
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = getRepository(User);
+  }
+
+  async findUserWithGamesById({
+    user_id,
+  }: IFindUserWithGamesDTO): Promise<User> {
+    const userWithGames = await this.repository.findOne( { id: user_id }, {relations: ['games']} );
+    
+    return userWithGames!;
+  }
+
+  async findAllUsersOrderedByFirstName(): Promise<User[]> {
+    return this.repository.query("SELECT * FROM users ORDER BY first_name"); // Complete usando raw query
+  }
+
+  async findUserByFullName({
+    first_name,
+    last_name,
+  }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
+
+    return this.repository.query(`SELECT * FROM users WHERE LOWER(first_Name) = LOWER('${first_name}') AND LOWER(last_name) = LOWER('${last_name.toLowerCase()}')`); // Complete usando raw query
+  }
+}
